e23977bf2f552b3b5f2b28172f7f067781613d44
hungnguyen2710 <hungnm.dev94@gmail.com>
2022-12-07 18:32:21 +0700

    code

--- Modules/Tiktok/Http/Controllers/TiktokController.php
+++ Modules/Tiktok/Http/Controllers/TiktokController.php
@@ -7,6 +7,7 @@ use DateTime;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpClient\HttpClient;
@@ -24,59 +25,85 @@ class TiktokController extends AppBaseController
             'url' => 'required'
             ]);
 
-        $headers = [
-            'sec-fetch-user' => '?1',
-            'sec-ch-ua-mobile' => '?0',
-            'sec-fetch-site' => 'none',
-            'sec-fetch-dest' => 'document',
-            'sec-fetch-mode' => 'navigate',
-            'cache-control' => 'max-age=0',
-            'authority' => 'https://www.tiktok.com/',
-            'upgrade-insecure-requests' => '1',
-            'accept-language' => 'en-GB,en;q=0.9,tr-TR;q=0.8,tr;q=0.7,en-US;q=0.6',
-            'sec-ch-ua' => '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'cookie' => 'sb=Rn8BYQvCEb2fpMQZjsd6L382; datr=Rn8BYbyhXgw9RlOvmsosmVNT; c_user=100003164630629; _fbp=fb.1.1629876126997.444699739; wd=1920x939; spin=r.1004812505_b.trunk_t.1638730393_s.1_v.2_; xs=28%3A8ROnP0aeVF8XcQ%3A2%3A1627488145%3A-1%3A4916%3A%3AAcWIuSjPy2mlTPuZAeA2wWzHzEDuumXI89jH8a_QIV8; fr=0jQw7hcrFdas2ZeyT.AWVpRNl_4noCEs_hb8kaZahs-jA.BhrQqa.3E.AAA.0.0.BhrQqa.AWUu879ZtCw',
+//        $headers = [
+//            'sec-fetch-user' => '?1',
+//            'sec-ch-ua-mobile' => '?0',
+//            'sec-fetch-site' => 'none',
+//            'sec-fetch-dest' => 'document',
+//            'sec-fetch-mode' => 'navigate',
+//            'cache-control' => 'max-age=0',
+//            'authority' => 'https://www.tiktok.com/',
+//            'upgrade-insecure-requests' => '1',
+//            'accept-language' => 'en-GB,en;q=0.9,tr-TR;q=0.8,tr;q=0.7,en-US;q=0.6',
+//            'sec-ch-ua' => '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+//            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+//            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+//            'cookie' => 'sb=Rn8BYQvCEb2fpMQZjsd6L382; datr=Rn8BYbyhXgw9RlOvmsosmVNT; c_user=100003164630629; _fbp=fb.1.1629876126997.444699739; wd=1920x939; spin=r.1004812505_b.trunk_t.1638730393_s.1_v.2_; xs=28%3A8ROnP0aeVF8XcQ%3A2%3A1627488145%3A-1%3A4916%3A%3AAcWIuSjPy2mlTPuZAeA2wWzHzEDuumXI89jH8a_QIV8; fr=0jQw7hcrFdas2ZeyT.AWVpRNl_4noCEs_hb8kaZahs-jA.BhrQqa.3E.AAA.0.0.BhrQqa.AWUu879ZtCw',
+//        ];
+//
+//        $redirectCheckClient = HttpClient::create([
+//            'headers' => $headers,
+//        ]);
+//        $url = $request->url;
+//        $resp = $this->getContent($url);
+//
+//        $client = HttpClient::create([
+//            'headers' => $headers,
+//        ]);
+//        $response = $client->request('GET', $request->url);
+//        $datas = $response->getContent();
+//
+//
+//
+//        $resp = $datas;
+//        //echo "$resp";
+//        $check = explode('"downloadAddr":"', $resp);
+//        if (count($check) > 1) {
+//            $contentURL = explode("\"", $check[1])[0];
+//            $contentURL = str_replace("\\u0026", "&", $contentURL);
+//            $contentURL = str_replace("\\u002F", "/", $contentURL);
+//            $thumb = explode("\"", explode('og:image" content="', $resp)[1])[0];
+//            $username = explode('"', explode('"uniqueId":"', $resp)[1])[0];
+//            $create_time = explode('"', explode('"createTime":"', $resp)[1])[0];
+//            $dt = new DateTime("@$create_time");
+//            $create_time = $dt->format("d M Y H:i:s A");
+//            $videoKey = $this->getKey($contentURL);
+//            $cleanVideo = "https://api2-16-h2.musical.ly/aweme/v1/play/?video_id=$videoKey&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4";
+//            $cleanVideo = $this->getContent($cleanVideo, true);
+//            $link = $this->downloadVideo($contentURL);
+//            $dataOutput = [];
+//            $dataOutput['links'] = [];
+//            $uuid = Uuid::uuid4()->toString();
+//            $dataOutput['title'] = $username . $uuid;
+//            $dataOutput['thumbnail'] = $thumb;
+//            $dataOutput['links'][] = [
+//                "url" => config('app.url'). 'storage/'. $link,
+//                "format" => 'hd',
+//                "type" => 'video',
+//                "size" => 'N/A',
+//            ];
+//
+//
+//            $dataOutput['time'] = '';
+//            return $this->responseAPI(true, '', $dataOutput, 200);
+//        }
+        $dataSend = [
+            'url' => $request->url,
+            'token' => ''
         ];
+        $response = Http::withHeaders([
+            'Accept'       => 'application/json',
+            'content-type' => 'application/json',
+        ])
+            ->post('https://ssyoutube.com/api/convert', $dataSend)->json();
 
-        $redirectCheckClient = HttpClient::create([
-            'headers' => $headers,
-        ]);
-        $url = $request->url;
-        $resp = $this->getContent($url);
-
-        $client = HttpClient::create([
-            'headers' => $headers,
-        ]);
-        $response = $client->request('GET', $request->url);
-        $datas = $response->getContent();
-
-
-
-        $resp = $datas;
-        //echo "$resp";
-        $check = explode('"downloadAddr":"', $resp);
-        if (count($check) > 1) {
-            $contentURL = explode("\"", $check[1])[0];
-            $contentURL = str_replace("\\u0026", "&", $contentURL);
-            $contentURL = str_replace("\\u002F", "/", $contentURL);
-            $thumb = explode("\"", explode('og:image" content="', $resp)[1])[0];
-            $username = explode('"', explode('"uniqueId":"', $resp)[1])[0];
-            $create_time = explode('"', explode('"createTime":"', $resp)[1])[0];
-            $dt = new DateTime("@$create_time");
-            $create_time = $dt->format("d M Y H:i:s A");
-            $videoKey = $this->getKey($contentURL);
-            $cleanVideo = "https://api2-16-h2.musical.ly/aweme/v1/play/?video_id=$videoKey&vr_type=0&is_play_url=1&source=PackSourceEnum_PUBLISH&media_type=4";
-            $cleanVideo = $this->getContent($cleanVideo, true);
-            $link = $this->downloadVideo($contentURL);
-            $dataOutput = [];
+        $dataOutput = [];
             $dataOutput['links'] = [];
             $uuid = Uuid::uuid4()->toString();
-            $dataOutput['title'] = $username . $uuid;
-            $dataOutput['thumbnail'] = $thumb;
+            $dataOutput['title'] = $uuid;
+            $dataOutput['thumbnail'] =  $response['thumb'] ? $response['thumb'] : '';
             $dataOutput['links'][] = [
-                "url" => config('app.url'). 'storage/'. $link,
+                "url" => $response['url'][0] ? $response['url'][0]['url'] : '',
                 "format" => 'hd',
                 "type" => 'video',
                 "size" => 'N/A',
@@ -85,7 +112,6 @@ class TiktokController extends AppBaseController
 
             $dataOutput['time'] = '';
             return $this->responseAPI(true, '', $dataOutput, 200);
-        }
     }
 
 
