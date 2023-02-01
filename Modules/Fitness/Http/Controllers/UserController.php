47e002d6a714547f84abba74d04939bb2372a487
BIDVAdmin <hungnm18@bidv.com.vn>
2023-02-01 15:29:57 +0700

    code

--- Modules/Fitness/Http/Controllers/UserController.php
+++ Modules/Fitness/Http/Controllers/UserController.php
@@ -181,7 +181,7 @@ class UserController extends AppBaseController
 //        $arrNumber = FitnessLogs::orderBy('created_at','DESC')->pluck('day_count')->toArray();
 //        sort($arrNumber);
         $arrNumber = [1,2,1,2,3];
-        array_unique($arrNumber);
-        return $this->responseAPI(true, '', $arrNumber, 200);
+       $test =  array_unique($arrNumber);
+        return $this->responseAPI(true, '', $test, 200);
     }
 }
