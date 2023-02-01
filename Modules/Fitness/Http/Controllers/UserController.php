bf71a10b2ac6036d6f90a589c3f3f35d176b40c7
BIDVAdmin <hungnm18@bidv.com.vn>
2023-02-01 15:21:30 +0700

    code

--- Modules/Fitness/Http/Controllers/UserController.php
+++ Modules/Fitness/Http/Controllers/UserController.php
@@ -179,7 +179,7 @@ class UserController extends AppBaseController
 
     public function chartLog(){
         $arrNumber = FitnessLogs::orderBy('created_at','DESC')->pluck('day_count');
-
+        $arrNumber = sort($arrNumber);
         return $this->responseAPI(true, '', $arrNumber, 200);
     }
 }
