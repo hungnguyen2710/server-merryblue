fc83d5a6ddced596b164abab6b6384eaf14495f9
hungnm18 <hungnm18@bidv.com.vn>
2023-06-01 11:20:11 +0700

    code

--- Modules/FakeMessage/Http/Controllers/FakeMessageController.php
+++ Modules/FakeMessage/Http/Controllers/FakeMessageController.php
@@ -66,7 +66,7 @@ class FakeMessageController extends AppBaseController
         $limit = isset($request->limit) ? $request->limit : 10;
         $offset = isset($request->offset) ? $request->offset : 0;
         $categoryCelebrityId = $request->category_id;
-        $celebrity = FakeMessageCelebrity::where('fake_message_category_celebrity_id', $categoryCelebrityId)->orderBy('created_at', 'DESC')->paginate($limit)->get();
+        $celebrity = FakeMessageCelebrity::where('fake_message_category_celebrity_id', $categoryCelebrityId)->orderBy('created_at', 'DESC')->paginate($limit);
 
         return $this->responseAPI(true, '', $celebrity, 200);
     }
